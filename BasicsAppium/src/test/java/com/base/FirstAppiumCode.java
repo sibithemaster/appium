671793196ba40565bf9f3eaf88d513467891480b
Green <evergreenmsd@gmail.com>
2023-06-12 16:28:57 +0530

    First commit

---
+++ BasicsAppium/src/test/java/com/base/FirstAppiumCode.java
@@ -0,0 +1,25 @@
+package com.base;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.testng.annotations.Test;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+public class FirstAppiumCode {
+
+	@Test
+	public void localDevice() throws MalformedURLException
+	{
+		
+		//UIAutomator
+		UiAutomator2Options options = new UiAutomator2Options();
+		options.setPlatformName("android");
+		options.setDeviceName("Pixel 2");
+		options.setApp("C:\\Users\\Sibisubramanian\\git\\appium\\BasicsAppium\\src\\test\\java\\com\\base\\FirstAppiumCode.java");
+		//Android driver setup
+		AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+	}
+}
