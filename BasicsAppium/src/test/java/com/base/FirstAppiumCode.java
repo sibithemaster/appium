f3cefaf616f5d803106c0bbd8a59c6b9f8255054
GowthamMura <124874885+GowthamMura@users.noreply.github.com>
2023-06-13 17:12:43 +0530

    Revert "add method"

--- BasicsAppium/src/test/java/com/base/FirstAppiumCode.java
+++ BasicsAppium/src/test/java/com/base/FirstAppiumCode.java
@@ -23,7 +23,7 @@ public class FirstAppiumCode {
 		AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
 	}
 	public void add() {
-		System.out.println("hello");
+		System.out.println("change to add");
 		
 	}
 }
